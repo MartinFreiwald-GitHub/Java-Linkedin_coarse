dd99a0314d411fa92d888678342e0977cf5c0841
MartinFreiwald-GitHub <MartinFreiwald@yahoo.com>
2019-07-09 15:55:39 -0700

    my java files in exercise
    my java files in exercise

---
+++ NumConversion.java
@@ -0,0 +1,19 @@
+public class NumConversion {
+    public static void main(String[] args)
+}
+        int  firstArg = 0;
+        if(args.length >0)
+        {
+            try {
+                firstArg= Integer.parseInt(args[]);
+            }
+            catch(NumberFormatException e){
+                System.err.println("Argument"+args[0] + " Must be an integer");
+                                                            System.exit(1);
+                                                        }
+                                                        System.out.println("You entered"+firstArg);
+                }           
+            }
+        }
+    }
+}    
